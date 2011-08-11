007c6cb719b0430d9170df32b1431778e45f0b99
rongjihuang <rongjihuang@gmail.com>
2011-08-12 00:13:01 +0800

    整合一个简单的activiti测试类

---
+++ bc-workflow-core/src/test/java/cn/bc/workflow/ActivitiTest.java
@@ -0,0 +1,42 @@
+package cn.bc.workflow;
+
+import static org.junit.Assert.*;
+
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.activiti.engine.test.Deployment;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration("classpath:spring-test.xml")
+public class ActivitiTest {
+	@Autowired
+	private RuntimeService runtimeService;
+
+	@Autowired
+	private TaskService taskService;
+
+	@Autowired
+	@Rule
+	public ActivitiRule activitiSpringRule;
+
+	@Test
+	@Deployment(resources = { "cn/bc/workflow/testProcess.bpmn20.xml" })
+	public void simpleProcessTest() {
+		runtimeService.startProcessInstanceByKey("testProcess");
+		Task task = taskService.createTaskQuery().singleResult();
+		assertEquals("My Task", task.getName());
+
+		taskService.complete(task.getId());
+		assertEquals(0, runtimeService.createProcessInstanceQuery().count());
+	}
+}
