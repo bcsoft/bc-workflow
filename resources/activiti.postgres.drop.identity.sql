5d78c5e0a43bcb4de31916ec935a6b3e587ce5f5
dragon <rongjihuang@gmail.com>
2012-07-03 13:58:57 +0800

    合并activiti的drop脚本

--- resources/activiti.postgres.drop.identity.sql
+++
@@ -1,4 +0,0 @@
-drop table if exists ACT_ID_INFO;
-drop table if exists ACT_ID_MEMBERSHIP;
-drop table if exists ACT_ID_GROUP;
-drop table if exists ACT_ID_USER;
---
+++ resources/activiti.postgres.drop.sql
@@ -0,0 +1,25 @@
+-- history
+drop table if exists ACT_HI_PROCINST;
+drop table if exists ACT_HI_ACTINST;
+drop table if exists ACT_HI_TASKINST;
+drop table if exists ACT_HI_DETAIL;
+drop table if exists ACT_HI_COMMENT;
+drop table if exists ACT_HI_ATTACHMENT;
+
+-- engine
+drop table if exists ACT_RU_JOB;
+drop table if exists ACT_RU_VARIABLE;
+drop table if exists ACT_RU_EVENT_SUBSCR;
+drop table if exists ACT_RU_IDENTITYLINK;
+drop table if exists ACT_RU_TASK;
+drop table if exists ACT_RU_EXECUTION;
+drop table if exists ACT_RE_PROCDEF;
+drop table if exists ACT_GE_BYTEARRAY;
+drop table if exists ACT_RE_DEPLOYMENT;
+drop table if exists ACT_GE_PROPERTY;
+
+-- identity
+drop table if exists ACT_ID_INFO;
+drop table if exists ACT_ID_MEMBERSHIP;
+drop table if exists ACT_ID_GROUP;
+drop table if exists ACT_ID_USER;
