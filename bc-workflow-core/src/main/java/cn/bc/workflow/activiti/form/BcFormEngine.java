dd013d9ca3f225d8e0c4fe1e7a7cd3ebe0508871
rongjihuang <rongjihuang@gmail.com>
2012-07-15 20:30:52 +0800

    表单引擎从类资源路径加载的方法封装

--- bc-workflow-core/src/main/java/cn/bc/workflow/activiti/form/BcFormEngine.java
+++ bc-workflow-core/src/main/java/cn/bc/workflow/activiti/form/BcFormEngine.java
@@ -1,5 +1,7 @@
 package cn.bc.workflow.activiti.form;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
@@ -18,6 +20,7 @@ import org.activiti.engine.impl.scripting.ScriptingEngines;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.FileCopyUtils;
 
 import cn.bc.core.exception.CoreException;
 import cn.bc.core.util.DateUtils;
@@ -206,20 +209,45 @@ public class BcFormEngine implements FormEngine {
 			String key) {
 		String sourceFormString;
 		if ("tpl".equalsIgnoreCase(from)) {// 从模板中加载资源
-			Template template = templateService.loadByCode(key);
-			if (template == null) {
-				throw new CoreException("can't find template:code=" + key);
-			}
-			if (!template.isPureText()) {
-				throw new CoreException("template isn't pure text:code=" + key);
-			}
-			sourceFormString = template.getContentEx();
+			sourceFormString = loadFormTemplateByTemplate(key);
 		} else if ("url".equalsIgnoreCase(from)) {// 使用URL方式 TODO
-			sourceFormString = "";
+			sourceFormString = loadFormTemplateByUrl(key);
+		} else if ("res".equalsIgnoreCase(from)) {// 从类资源中获取
+			sourceFormString = loadFormTemplateByClassResource(key);
 		} else {// 默认使用activiti的方法加载资源
 			sourceFormString = loadFormTemplateByActivitiDefault(formInstance,
 					key);
 		}
+		return sourceFormString;
+	}
+
+	private String loadFormTemplateByClassResource(String key) {
+		// 获取文件流
+		InputStream resFile = this.getClass().getResourceAsStream("/" + key);
+		try {
+			return new String(FileCopyUtils.copyToByteArray(resFile));
+		} catch (IOException e) {
+			logger.warn(e.getMessage(), e);
+			throw new CoreException(e.getMessage(), e);
+		}
+	}
+
+	private String loadFormTemplateByUrl(String key) {
+		// TODO 
+		// 如果直接使用HttpClient或Jsoup对URL执行请求，session的同步需要处理
+		throw new CoreException("unsupport method:loadFormTemplateByUrl");
+	}
+
+	private String loadFormTemplateByTemplate(String key) {
+		String sourceFormString;
+		Template template = templateService.loadByCode(key);
+		if (template == null) {
+			throw new CoreException("can't find template:code=" + key);
+		}
+		if (!template.isPureText()) {
+			throw new CoreException("template isn't pure text:code=" + key);
+		}
+		sourceFormString = template.getContentEx();
 		return sourceFormString;
 	}
 
