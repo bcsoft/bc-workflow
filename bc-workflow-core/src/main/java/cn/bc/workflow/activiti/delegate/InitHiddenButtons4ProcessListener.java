ace08f79e1ce8f38de054eede29044537cb29a87
fatfish <fatfish2010@sina.cn>
2012-10-25 13:59:39 +0800

    增加隐藏“添加意见，添加附件”按钮的监听器

---
+++ bc-workflow-core/src/main/java/cn/bc/workflow/activiti/delegate/InitHiddenButtons4ProcessListener.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package cn.bc.workflow.activiti.delegate;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.ExecutionListener;
+import org.activiti.engine.impl.el.Expression;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * 
+ * 初始化流程公共信息区‘添加附件,添加意见’等按钮默认隐藏控制的监听器 
+ * <p>
+ * 只能配置在流程的启动事件中
+ * </p>
+ * @author lbj
+ * 
+ */
+public class InitHiddenButtons4ProcessListener implements ExecutionListener {
+	private static final Log logger = LogFactory
+			.getLog(InitHiddenButtons4ProcessListener.class);
+
+	private Expression hiddenButtonCodes; //需要默认隐藏按钮的编码，例如BUTTON_ADDCOMMENT,BUTTON_ADDATTACH 多个逗号隔开
+
+	public void notify(DelegateExecution execution) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("hiddenButtonCodes="
+					+ (hiddenButtonCodes != null ? hiddenButtonCodes.getExpressionText() : null));
+			logger.debug("processInstanceId=" 
+					+ execution.getProcessInstanceId());
+			logger.debug("eventName=" + execution.getEventName());
+		}
+		
+		if(hiddenButtonCodes != null && hiddenButtonCodes.getExpressionText() != ""){
+			execution.setVariable("hiddenButtonCodes", hiddenButtonCodes.getExpressionText());
+		}
+	}
+}
---
+++ bc-workflow-core/src/main/java/cn/bc/workflow/activiti/delegate/InitHiddenButtons4TaskListener.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package cn.bc.workflow.activiti.delegate;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+import org.activiti.engine.impl.el.Expression;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * 
+ * 初始化待办区任务的‘添加附件,添加意见’等按钮默认隐藏控制的监听器 
+ * 
+ * @author lbj
+ * 
+ */
+public class InitHiddenButtons4TaskListener implements TaskListener {
+	private static final Log logger = LogFactory
+			.getLog(InitHiddenButtons4TaskListener.class);
+
+	private Expression hiddenButtonCodes; //需要默认隐藏按钮的编码，例如BUTTON_ADDCOMMENT,BUTTON_ADDATTACH 多个逗号隔开
+
+	public void notify(DelegateTask delegateTask) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("hiddenButtonCodes="
+					+ (hiddenButtonCodes != null ? hiddenButtonCodes.getExpressionText() : null));
+			logger.debug("processInstanceId=" 
+					+ delegateTask.getProcessInstanceId());
+			logger.debug("taskDefinitionKey="
+					+ delegateTask.getTaskDefinitionKey());
+			logger.debug("taskId=" 
+					+ delegateTask.getId());
+			logger.debug("eventName=" + delegateTask.getEventName());
+		}
+		
+		if(hiddenButtonCodes != null && hiddenButtonCodes.getExpressionText() != ""){
+			delegateTask.setVariableLocal("hiddenButtonCodes", hiddenButtonCodes.getExpressionText());
+		}
+	}
+
+}
